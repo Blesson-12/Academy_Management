0d332e16132939eab185694af16cfc5b0c6fb263
Blesson N <110018243+Blesson-12@users.noreply.github.com>
2026-01-12 12:43:13 +0530

    Change MongoDB connection to use Atlas
    Updated MongoDB connection string to use Atlas.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const PORT = 5000;
 app.use(cors())
 app.use(express.json())
-mongoose.connect("mongodb://127.0.0.1:27017/successacademy")
+mongoose.connect("mongodb+srv://blessonnelson12_db_user:<Blesson12345>@cluster0.nroiue0.mongodb.net/?appName=Cluster0/successacademy")
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log("MongoDB error:", err));
 app.use('/admin',authorization)
@@ -23,4 +23,5 @@ app.listen(PORT,(err)=>{
     console.log(err.message)
     }else
     console.log(`Server is running on port ${PORT}`);
+
 })
